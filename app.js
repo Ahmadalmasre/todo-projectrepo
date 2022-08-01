8bbc020155dff4c9a51437071cba5402f4d2f7ac
Ahmadalmasre <ahmad_almasri96@yahoo.com>
2022-08-02 00:11:18 +0300

    sff

---
+++ app.js
@@ -0,0 +1,52 @@
+//prompt("ادخل اسمك")
+if(confirm("هل تريد تخطي رسائل الترحيب؟")){
+alert("Welcome in us page")
+var userName =prompt("ادخل اسمك");
+
+
+
+
+//prompt("ادخل جنسك")
+
+
+var Age=prompt("عمرك");
+if(Age<=0){
+    alert("الرجاء ادخال عمرك الحقيقي");
+    Age=prompt("عمرك", "write mail or femail");
+
+}
+var ginder =prompt("ادخل جنسك", "write mail or femail");
+}else{
+    alert("استمتع بموقعنا") 
+}
+
+var Array =[];
+if(confirm("هل وجدت ضالتك")){
+    Array[0]="Yes";
+}else{
+    Array[0]="no";
+}
+
+if(confirm("هل استمتعت معنا")){
+    Array[1]="Yes";
+}else{
+    Array[1]="no";
+}
+
+if(confirm("هل سنراك مجددا")){
+    Array[2]="Yes";
+}else{
+    Array[2]="no";
+}
+console.log("هل وجدت ضالتك"+A[0]);
+console.log("هل استمتعت معنا"+A[1]);
+console.log("هل سنراك مجددا"+A[1]);
+
+
+
+
+
+
+
+
+
